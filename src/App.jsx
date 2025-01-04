08e1244e4816f48d701b584b91e83a8debeeb2da
sabbir1257 <masabbir1257@gmail.com>
2025-01-04 23:51:13 +0600

    JSX, its rules and add dynamic content to JSX in your first component

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,28 @@ function App() {
   return (
     <>
       <h1>Vite + React</h1>
+      <Person />
+      <Person />
+      <Person />
+      <Student />
     </>
+  )
+}
+
+function Person() {
+  const age = 25;
+  const money = 20;
+  const person = { name: 'sakib', age: 12 };
+  return <h3>I am {person.name} person with age: {age + money}</h3>
+}
+
+function Student() {
+  return (
+    <div>
+      <h3>This is a student</h3>
+      <p>Name: </p>
+      <p>age: </p>
+    </div>
   )
 }
 
