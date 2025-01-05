155d7d20894805670eb206ab671a25a72cbfaaad
sabbir1257 <masabbir1257@gmail.com>
2025-01-05 21:16:58 +0600

    Explore JSX and Props basics

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
     <>
       <h1>Vite + React</h1>
+      <Device name="Laptop" price="55"/>
+      <Device name="Mobile" price="17"/>
+      <Device name="Watch" price="3"/>
       <Person />
-      <Person />
-      <Person />
+      <Student grade="7" score="99" />
       <Student />
+      <Student />
+      <Developer/>
     </>
   )
+}
+
+function Device(props){
+  // console.log(props);
+  return <h2>This device: {props.name} price: {props.price}</h2>
 }
 
 function Person() {
@@ -23,9 +28,10 @@ function Person() {
   return <h3>I am {person.name} person with age: {age + money}</h3>
 }
 
-function Student() {
+function Student(props) {
+  console.log(props)
   return (
-    <div>
+    <div className='student'>
       <h3>This is a student</h3>
       <p>Name: </p>
       <p>age: </p>
@@ -33,4 +39,20 @@ function Student() {
   )
 }
 
-export default App
+function Developer(){
+  const developerStyle = {
+    margin: '20px',
+    padding: '20px',
+    border: '2px solid purple',
+    borderRadius: '20px'
+
+  }
+  return (
+    <div style={developerStyle}>
+      <h5>Devo devo</h5>
+      <p>Coding: </p>
+    </div>
+  )
+}
+
+export default App;
