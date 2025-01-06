20cf35ecbfb8ee4c1661a0ac12537f4050e354f4
sabbir1257 <masabbir1257@gmail.com>
2025-01-06 14:46:29 +0600

    Read only props Two way of conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
 import './App.css';
+import Todo from './Todo';
 
 function App() {
   return (
     <>
-      <h1>Vite + React</h1>
-      <Device name="Laptop" price="55"/>
+      <h3>Vite + React</h3>
+      <Todo task="Learn React" isDone={true} />
+      <Todo task="Core concepts" isDone={false} />
+      <Todo task="Try JSX" isDone={true} />
+      {/* <Device name="Laptop" price="55"/>
       <Device name="Mobile" price="17"/>
       <Device name="Watch" price="3"/>
       <Person />
       <Student grade="7" score="99" />
+      <Student grade={12} score="85" />
       <Student />
-      <Student />
-      <Developer/>
+      <Developer/> */}
     </>
   )
 }
 
-function Device(props){
+function Device(props) {
   // console.log(props);
   return <h2>This device: {props.name} price: {props.price}</h2>
 }
@@ -28,24 +32,26 @@ function Person() {
   return <h3>I am {person.name} person with age: {age + money}</h3>
 }
 
-function Student(props) {
-  console.log(props)
+const { grade, score } = { grade: '7', score: '99' };
+
+function Student({ grade = 1, score = 0 }) {
+  console.log(grade, score);
+  // console.log(props)
   return (
     <div className='student'>
       <h3>This is a student</h3>
-      <p>Name: </p>
-      <p>age: </p>
+      <p>Class: {grade}</p>
+      <p>score: {score}</p>
     </div>
   )
 }
 
-function Developer(){
+function Developer() {
   const developerStyle = {
     margin: '20px',
     padding: '20px',
     border: '2px solid purple',
     borderRadius: '20px'
-
   }
   return (
     <div style={developerStyle}>
